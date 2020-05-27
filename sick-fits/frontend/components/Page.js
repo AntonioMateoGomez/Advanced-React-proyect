87ed09736d4a07cff52c89c5d0986d257f68d973
AntonioMateoGomez <antonio.mateo@47deg.com>
2020-05-27 10:38:38 +0200

    Settup Apollo provider and update styled-components

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styled, { ThemeProvider, injectGlobal } from "styled-components";
+import styled, { ThemeProvider, createGlobalStyle } from "styled-components";
 import Header from "../components/Header";
 import Meta from "../components/Meta";
 
@@ -24,7 +24,7 @@ const Inner = styled.div`
   padding: 2rem;
 `;
 
-injectGlobal`
+const GlobalStyles = createGlobalStyle`
   @font-face {
     font-family: "radnika_next";
     src: url("/static/radnikanext-medium-webfont.woff2")
@@ -56,6 +56,7 @@ class Page extends React.Component {
   render() {
     return (
       <ThemeProvider theme={theme}>
+        <GlobalStyles />
         <StyledPage>
           <Meta />
           <Header />
